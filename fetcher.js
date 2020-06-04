467aed62fd389e3deec2df4bc7d8464715879e16
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-06-04 06:46:21 +0000

    Fixed some of the edge cases.

--- fetcher.js
+++ fetcher.js
@@ -4,18 +4,20 @@ const fs = require('fs');
 
 const request = require('request');
 request(reqArray[0], function(error, response, body) {
-  fs.stat(reqArray[1], (error, stats) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(`Downloaded and saved ${stats.size} bytes to ${reqArray[1]}`);
-    }
-  });
-
-  
-  fs.writeFile(reqArray[1], body, function(err) {
-    if (err) {
-      return console.log(err);
-    }
-  });
+  if (error !== null) {
+    console.log('Error: The URL is not valid.');
+  } else {
+    fs.stat(reqArray[1], (error, stats) => {
+      if (error) {
+        console.log('Error: File path does not exist.');
+      } else {
+        fs.writeFile(reqArray[1], body, function(err) {
+          if (err) {
+            return console.log(err);
+          }
+        });
+        console.log(`Downloaded and saved ${stats.size} bytes to ${reqArray[1]}`);
+      }
+    });
+  }
 });
